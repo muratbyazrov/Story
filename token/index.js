aa8367d02a8f036f9c85667533aa6df3ef42cde4
murat <murat.byazrov@yandex.ru>
2023-01-31 11:59:22 +0800

    new version

--- token/index.js
+++ token/index.js
@@ -5,21 +5,26 @@ const {Forbidden} = require("../errors");
 class Token {
     generateToken(config, data) {
         const {key, expiresIn = 24 * 60 * 60 * 1000} = config.token;
-        return jwt.sign({...data}, key, {algorithm: 'RS256', expiresIn}, (error) => {
-            throw new Forbidden(error);
+        return jwt.sign({...data}, key, {algorithm: 'RS256', expiresIn}, (error, token) => {
+            if (token) {
+                console.log(token);
+            }
+            logger.error(error);
+            throw new Forbidden(error.message);
         });
     }
 
     decodeToken(config, token) {
         const {key} = config.token;
         return jwt.verify(token, key, {algorithm: 'RS256'}, (error) => {
-            throw new Forbidden(error);
+            logger.error(error);
+            throw new Forbidden(error.message);
         })
     }
 
     checkToken(config, {checkToken = true, token}) {
         if (!checkToken) {
-            return;
+            return true;
         }
         if (!token) {
             throw new Forbidden('Token must be specified');
