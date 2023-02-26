5a2b1a0002481546fd70236ea7ea03e6c917ee63
murat <murat.byazrov@yandex.ru>
2023-02-26 20:57:39 +0800

    add token error

--- response/index.js
+++ response/index.js
@@ -1,5 +1,5 @@
 class Response {
-    format({domain = 'error', event = 'error'}, data) {
+    format({domain = 'error', event = 'error'}, data = {}) {
         const {isError, ..._data} = data;
         return isError ? {domain, event, error: _data} : {domain, event, data};
     }
