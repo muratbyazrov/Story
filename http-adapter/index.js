7676c4b96c5e3fae5c0f968541e460e1b6958027
murat <murat.byazrov@yandex.ru>
2023-04-05 16:00:14 +0800

    http module downloads

--- http-adapter/index.js
+++ http-adapter/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const {logger} = require('../logger');
 const multer = require('multer');
 const path = require('path');
+const mime = require('mime-types');
 
 class HttpAdapter {
     constructor(options) {
@@ -11,14 +12,25 @@ class HttpAdapter {
     }
 
     run(callback) {
-        app.listen(this.config.port, this.config.host, () => {
-            logger.info(`App listening http (${this.config.host}:${this.config.port})`);
+        const {host, port, processFiles = false} = this.config;
+        app.listen(port, host, () => {
+            logger.info(`App listening http (${host}:${port})`);
         });
         app.use(bodyParser.json());
         app.post(this.config.path, async (req, res) => res.send(await callback(req.body)));
+        processFiles && this.processFiles(callback);
+    }
 
+    processFiles(callback) {
         const {uploadsPath = `${this.config.path}/uploads`, downloadsPath = `${this.config.path}/downloads`} = this.config;
-        const upload = multer({dest: 'uploads/'}).any();
+        const storage = multer.diskStorage({
+            destination: 'uploads/',
+            filename: function (req, file, cb) {
+                cb(null, `${file.fieldname}-${Date.now()}.${mime.extension(file.mimetype)}`);
+            }
+        });
+        const upload = multer({ storage }).any();
+
         app.use(uploadsPath, async (req, res, next) => {
             const result = await callback({...req.headers, params: {}});
             result.error && res.send(result);
