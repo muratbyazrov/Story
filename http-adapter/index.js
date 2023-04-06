9559a214876a923efd13dc4447214af8d7d25021
murat <murat.byazrov@yandex.ru>
2023-04-06 13:23:25 +0800

    http module downloads

--- http-adapter/index.js
+++ http-adapter/index.js
@@ -22,23 +22,22 @@ class HttpAdapter {
     }
 
     processFiles(callback) {
-        const {uploadsPath = `${this.config.path}/uploads`, downloadsPath = `${this.config.path}/downloads`} = this.config;
+        const {
+            uploadsPath = `${this.config.path}/uploads`,
+            downloadsPath = `${this.config.path}/downloads`
+        } = this.config;
         const storage = multer.diskStorage({
             destination: 'uploads/',
             filename: function (req, file, cb) {
                 cb(null, `${file.fieldname}-${Date.now()}.${mime.extension(file.mimetype)}`);
             }
         });
-        const upload = multer({ storage }).any();
-
-        app.use(uploadsPath, async (req, res, next) => {
-            const result = await callback({...req.headers, params: {}});
-            result.error && res.send(result);
-            next();
-        });
+        const upload = multer({storage}).any();
         app.post(uploadsPath, upload, async (req, res) => {
             const {domain, event, token} = req.headers;
-            res.send(await callback({domain, event, token, params: {files: req.files}}));
+            const result = await callback({domain, event, params: {files: req.files}, token});
+            // Если ошибка - удалить файл: result.error &&...
+            res.send(result);
         });
         app.use(downloadsPath, express.static(`${path.dirname(require.main.filename)}/uploads`));
     }
