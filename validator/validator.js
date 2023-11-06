3c4edf5e1ca30a3501012ba3842cfd001966424a
muratbyazrov <murat.byazrov@yandex.ru>
2023-11-06 21:03:23 +0600

    config refactoring

--- validator/validator.js
+++ validator/validator.js
@@ -13,7 +13,7 @@ class Validator {
             array: {type: 'array'},
             array1: {type: 'array', minItems: 1},
             boolean: {type: 'boolean'},
-            empty: {type: 'null'},
+            null: {type: 'null'},
         };
     }
 
